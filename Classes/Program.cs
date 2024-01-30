67da6371bf9701b2910254bfccf33b413ec8edb1
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2024-01-30 14:12:00 +0100

    01-Classes finish

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Car
+{
+    /*Make a Car Object with 3 properties ( Brand, Model, Price )
+      This class does need to have TWO ways of creating a car object
+      Find a way to show the made object in the console
+    */
+
+    // Properties
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public double Price { get; set; }
+    //Ctor to initialize a car object on the same line without dot notation
+    public Car(string brand, string model, double price)
+    {
+        Brand = brand;
+        Model = model;
+        Price = price;
+    }
+    //Ctor to create a car object to initialize it later with dot notation
+    public Car()
+    {
+        
+    }
+
+    // Printing car details
+    public void PrintCarDetails()
+    {
+        Console.WriteLine($"[BRAND]:{Brand}\n\t[MODEL]:{Model}\n\t[PRICE]:{Price}\n");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Car WITH ctor
+        Car carWithCtor = new Car("Mercedes","GLA",85600);
+        // Print car details
+        carWithCtor.PrintCarDetails();
+        // Car WITHOUT ctor
+        Car carWithoutCtor = new Car();
+        carWithoutCtor.Brand = "Porche";
+        carWithoutCtor.Model = "Boxster";
+        carWithoutCtor.Price = 78500;
+        // Print car details
+        carWithoutCtor.PrintCarDetails();
+    }
+}
