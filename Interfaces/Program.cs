bee3355a434d9ec1d72f51fb3e3856bfb540976b
Y1lmaz-Ahmet <y.ahmet@outlook.be>
2024-02-01 13:50:48 +0100

    interfaces finish & 03- acces modifier readme.md name change

---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+public interface IDrawable
+{
+    void Draw();
+}
+public class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a circle.");
+    }
+}
+public class Square : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a square");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<IDrawable> shapes = new List<IDrawable>
+        {
+            new Circle(),
+            new Square(),
+        };
+
+        foreach (IDrawable shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
